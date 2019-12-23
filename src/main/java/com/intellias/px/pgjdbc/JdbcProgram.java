750c51acaa61bb5864bee761547c1d3068548fd1
bbrutall <gnatenkovadim@gmail.com>
2019-12-24 00:01:46 +0200

    fixup! upd

--- src/main/java/com/intellias/px/pgjdbc/JdbcProgram.java
+++ src/main/java/com/intellias/px/pgjdbc/JdbcProgram.java
@@ -1,4 +1,19 @@
 package com.intellias.px.pgjdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JdbcProgram {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/project-x-db");
+        Statement statement = connection.createStatement();
+        int updateCount = statement.executeUpdate("create table users (id integer, name varchar(255), password varchar(255))");
+        System.out.println("Updated" + updateCount);
+
+//        statement.executeUpdate("insert into users (id, name, password) values (0, admin, 12345)");
+        statement.close();
+
+    }
 }
